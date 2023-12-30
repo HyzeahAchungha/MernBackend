d4c62f6f5862ba09a1190bfb14e9c500dac6a0de
kuku67011 <hyzeala28@gmail.com>
2023-12-31 00:43:41 +0100

    second commit

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express=require('express')
-const PORT=4000;
 const app=express('');
 const cors=require('cors')
 const mongoose=require('mongoose')
-
-const MONGO_URI = 'mongodb+srv://Hyzeah:wpOLq80kZ8PtpobJ@cluster0.iig3hxs.mongodb.net/?retryWrites=true&w=majority';
+const dotenv = require('dotenv')
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+dotenv.config()
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -26,12 +25,12 @@ app.use( "/api",routes)
 
 
 const start = () => {
-	mongoose.connect(MONGO_URI, (errr) => {
+	mongoose.connect(process.env.MONGO_Atlas, (errr) => {
 		if (errr) {
 			return console.log('Failed to connect to mongoDB');
 		}
 		console.log('Connected to DB.....');
 	});
- app.listen(PORT,()=>console.log(`server is runing on ${PORT}`))
+ app.listen(process.env.PORT,()=>console.log(`server is runing on ${process.env.PORT}`))
 }
  start()

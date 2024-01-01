173b12614aece9471599c72c5b1aca2052060f94
kuku67011 <hyzeala28@gmail.com>
2024-01-01 15:53:08 +0100

    update backend again

--- server.js
+++ server.js
@@ -13,7 +13,22 @@ app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
-
+// Enable CORS middleware
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', 'http://hyzeal-todo-app-98ee5043d3e9.herokuapp.com');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+  });
+  // Proxy route
+app.post('/api/save', async (req, res) => {
+	try {
+	  const response = await axios.post('http://localhost:4000/api/save', req.body);
+	  res.json(response.data);
+	} catch (error) {
+	  res.status(500).json({ error: 'An error occurred' });
+	}
+  });
 
 const routes =require('./Routes/TaskRoutes')
 
